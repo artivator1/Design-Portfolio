592e26a208f5a8fd7048c36d96dc90d300873ffa
artivator1 <ayomideadegboro116@gmail.com>
2026-09-23 13:07:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 const themeToggle = document.getElementById("themeToggle");
-const themeIcon = document.querySelector(".theme-icon");
+const themeIcon = document.getElementById("themeIcon");
 
 const savedTheme = localStorage.getItem("theme");
 
-
 if (savedTheme === "light") {
-
     document.body.classList.remove("dark");
-
     themeIcon.textContent = "◐";
-
 } else {
-
     document.body.classList.add("dark");
-
     themeIcon.textContent = "☼";
-
     localStorage.setItem("theme", "dark");
-
 }
 
 
@@ -25,9 +17,7 @@ themeToggle.addEventListener("click", () => {
 
     document.body.classList.toggle("dark");
 
-    const isDark =
-        document.body.classList.contains("dark");
-
+    const isDark = document.body.classList.contains("dark");
 
     if (isDark) {
 
