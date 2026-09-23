9f30cb7145bb20ef15ebdef166e07468a2f10d44
artivator1 <ayomideadegboro116@gmail.com>
2026-09-23 12:47:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,23 +3,50 @@ const themeIcon = document.querySelector(".theme-icon");
 
 const savedTheme = localStorage.getItem("theme");
 
-if (savedTheme === "dark") {
+
+if (savedTheme === "light") {
+
+    document.body.classList.remove("dark");
+
+    themeIcon.textContent = "◐";
+
+} else {
+
     document.body.classList.add("dark");
+
     themeIcon.textContent = "☼";
+
+    localStorage.setItem("theme", "dark");
+
 }
+
 
 themeToggle.addEventListener("click", () => {
 
     document.body.classList.toggle("dark");
 
-    const isDark = document.body.classList.contains("dark");
+    const isDark =
+        document.body.classList.contains("dark");
+
 
     if (isDark) {
+
         themeIcon.textContent = "☼";
-        localStorage.setItem("theme", "dark");
+
+        localStorage.setItem(
+            "theme",
+            "dark"
+        );
+
     } else {
+
         themeIcon.textContent = "◐";
-        localStorage.setItem("theme", "light");
+
+        localStorage.setItem(
+            "theme",
+            "light"
+        );
+
     }
 
 });
